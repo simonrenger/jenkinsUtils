c77664ad6cfe83ee187f9fa5da3f644a50298feb
Simon Renger <simon.renger@gmail.com>
2020-03-18 12:47:33 +0100

    fix?

--- vars/notifReviewFailed.groovy
+++ vars/notifReviewFailed.groovy
@@ -6,6 +6,6 @@ WEBHOOK
             sendDiscord(createDiscordMessage(":x: Build #${env.BUILD_NUMBER} - Review: ${env.P4_REVIEW} Failed Building","failed",[
                 [name:"Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true],
                 [name:"Perforce Review",value:"${env.P4_REVIEW}",inline:true]
-            ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${REVIEW_URL}${env.P4_REVIEW}"),
-            WEBHOOK)  
+            ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${REVIEW_URL}${env.P4_REVIEW}")
+            ,WEBHOOK)
 }
