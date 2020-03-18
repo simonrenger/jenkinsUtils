69e7406fd1dda13c025c49b7bd47518b00b971d7
Simon Renger <simon.renger@gmail.com>
2020-03-18 12:53:43 +0100

    changed name accord to change in discord.groovy

--- vars/notifReviewSuccess.groovy
+++ vars/notifReviewSuccess.groovy
@@ -2,8 +2,8 @@ def call(
 REVIEW_URL,
 WEBHOOK
 ){
-    sendDiscord(
-        createDiscordMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Review: ${env.P4_REVIEW} Passed Building","ok",[
+    discord.send(
+        discord.createMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Review: ${env.P4_REVIEW} Passed Building","ok",[
                     [name:"Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true],
                     [name:"Perforce Review",value:"${env.P4_REVIEW}",inline:true]
                 ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL} Review Url: ${REVIEW_URL}${env.P4_REVIEW}")
