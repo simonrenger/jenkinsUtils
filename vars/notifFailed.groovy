6eaec021c11757d04cdcef079da01607b18f0730
Simon Renger <simon.renger@gmail.com>
2020-03-18 15:19:01 +0100

    fixed P4_CHANGELIST

--- vars/notifFailed.groovy
+++ vars/notifFailed.groovy
@@ -5,6 +5,6 @@ WEBHOOK
     discord.send(
              discord.createMessage(":x: Build #${env.BUILD_NUMBER} - Last Perforce Revision: ${env.P4_CHANGELIST} Failed Building","failed",[
                 [name:"Last Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true]
-            ],"${CHANGE_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${CHANGE_URL}${env.P4_CHANGELIST}")
+            ],"${CHANGE_URL}${env.P4_CHANGELIST}","Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${CHANGE_URL}${env.P4_CHANGELIST}")
             ,WEBHOOK)
 }
