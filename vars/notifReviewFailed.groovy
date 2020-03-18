6053cee4757ac483b89bee24ace272b01d8fb41d
Simon Renger <simon.renger@gmail.com>
2020-03-18 13:54:22 +0100

    fixed bug maybe

--- vars/notifReviewFailed.groovy
+++ vars/notifReviewFailed.groovy
@@ -3,7 +3,7 @@ REVIEW_URL,
 WEBHOOK
 ){
     discord.send(
-             discord.createMessage(createDiscordMessage(":x: Build #${env.BUILD_NUMBER} - Review: ${env.P4_REVIEW} Failed Building","failed",[
+             discord.createMessage(":x: Build #${env.BUILD_NUMBER} - Review: ${env.P4_REVIEW} Failed Building","failed",[
                 [name:"Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true],
                 [name:"Perforce Review",value:"${env.P4_REVIEW}",inline:true]
             ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${REVIEW_URL}${env.P4_REVIEW}")
