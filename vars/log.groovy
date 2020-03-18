c5dd960ca25357ebef21a1ca4205b150bb4db873
Simon Renger <simon.renger@gmail.com>
2020-03-18 14:04:00 +0100

    add log facility

---
+++ vars/log.groovy
@@ -0,0 +1,32 @@
+def setup(rulefile = 'msbuildparserules.txt'){
+    if (!fileExists(rulefile)) {
+writeFile file: rulefile, text: '''
+# Divide into sections based on project compile start
+start /BUILD COMMAND/
+start /COOK COMMAND/
+# Compiler Error
+error /(?i)error [A-Z]+[0-9]+:/
+error /(?i)Error:/
+# Compiler Warning
+warning /(?i)warning [A-Z]+[0-9]+:/
+warning /(?i)Warning:/
+warning /Couldn't find/
+'''
+    }
+}
+
+def warning(message){
+    echo "Warning: ${message}"
+}
+
+def error(message){
+    echo "Error: ${message}"
+}
+
+def info(message){
+    echo "Info: ${message}"
+}
+
+def parse(projectRulePath = 'msbuildparserules.txt',parsingRulesPath = 'msbuildparserules.txt',showGraphs = true){
+    logParser(projectRulePath:projectRulePath, parsingRulesPath: parsingRulesPath , showGraphs: showGraphs, useProjectRule: true)
+}
