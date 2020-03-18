7984378e5eb09c6c08a52819221b0aac335c69e8
Simon Renger <simon.renger@gmail.com>
2020-03-18 15:18:38 +0100

    added notify without review

---
+++ vars/notifFailed.groovy
@@ -0,0 +1,10 @@
+def call(
+CHANGE_URL,
+WEBHOOK
+){
+    discord.send(
+             discord.createMessage(":x: Build #${env.BUILD_NUMBER} - Last Perforce Revision: ${env.P4_CHANGELIST} Failed Building","failed",[
+                [name:"Last Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true]
+            ],"${CHANGE_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${CHANGE_URL}${env.P4_CHANGELIST}")
+            ,WEBHOOK)
+}
---
+++ vars/notifSuccess.groovy
@@ -0,0 +1,10 @@
+def call(
+CHANGE_URL,
+WEBHOOK
+){
+    discord.send(
+        discord.createMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Last Perforce Revision: ${env.P4_CHANGELIST} Passed Building","ok",[
+                    [name:"Last Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true]
+                ],"${CHANGE_URL}${env.P4_CHANGELIST}","Build URL: ${env.BUILD_URL} Change Url: ${CHANGE_URL}${env.P4_CHANGELIST}")
+                ,WEBHOOK)
+}
