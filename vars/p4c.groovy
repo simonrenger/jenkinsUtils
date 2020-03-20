0c7fe4426697416f776d153a802d868682b7bffe
Simon Renger <simon.renger@gmail.com>
2020-03-20 10:11:51 +0100

    added proper import

--- vars/p4c.groovy
+++ vars/p4c.groovy
@@ -1,4 +1,4 @@
-
+import groovy.json.JsonSlurper
 def getChangelistDescription(id,credential,client,view_mapping){
     def p4s = p4(credential: credential, workspace: manualSpec(charset: 'none', cleanup: false, name: client, pinHost: false, spec: clientSpec(allwrite: false, backup: true, changeView: '', clobber: false, compress: false, line: 'LOCAL', locked: false, modtime: false, rmdir: false, serverID: '', streamName: '', type: 'WRITABLE', view: view_mapping)))
     def list = p4s.run('describe', '-s', '-S', "${id}")
