101b9afeb9283359d6405909a71a708fd36ec42b
Simon Renger <simon.renger@gmail.com>
2020-03-21 19:59:03 +0100

    added missing new line

--- vars/notifReviewFailed.groovy
+++ vars/notifReviewFailed.groovy
@@ -7,6 +7,6 @@ description = ""
              discord.createMessage(":x: Build #${env.BUILD_NUMBER} - Review: ${env.P4_REVIEW} Failed Building","failed",[
                 [name:"Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true],
                 [name:"Perforce Review",value:"${env.P4_REVIEW}",inline:true]
-            ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${REVIEW_URL}${env.P4_REVIEW}Review Description:\n${description}")
+            ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${REVIEW_URL}${env.P4_REVIEW}\nReview Description:\n${description}")
             ,WEBHOOK)
 }
