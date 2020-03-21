da93d1c814528f57b39acc996319b58bfd7f0bd1
Simon Renger <simon.renger@gmail.com>
2020-03-21 19:52:33 +0100

    added addtional description parameter

--- vars/notifReviewFailed.groovy
+++ vars/notifReviewFailed.groovy
@@ -1,11 +1,12 @@
 def call(
 REVIEW_URL,
-WEBHOOK
+WEBHOOK,
+description = ""
 ){
     discord.send(
              discord.createMessage(":x: Build #${env.BUILD_NUMBER} - Review: ${env.P4_REVIEW} Failed Building","failed",[
                 [name:"Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true],
                 [name:"Perforce Review",value:"${env.P4_REVIEW}",inline:true]
-            ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${REVIEW_URL}${env.P4_REVIEW}")
+            ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${REVIEW_URL}${env.P4_REVIEW}Review Description:\n${description}")
             ,WEBHOOK)
 }
--- vars/notifReviewSuccess.groovy
+++ vars/notifReviewSuccess.groovy
@@ -1,11 +1,12 @@
 def call(
 REVIEW_URL,
-WEBHOOK
+WEBHOOK,
+description = ""
 ){
     discord.send(
         discord.createMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Review: ${env.P4_REVIEW} Passed Building","ok",[
                     [name:"Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true],
                     [name:"Perforce Review",value:"${env.P4_REVIEW}",inline:true]
-                ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL} Review Url: ${REVIEW_URL}${env.P4_REVIEW}")
+                ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${env.P4_REVIEW}\nReview Description:\n${description}")
                 ,WEBHOOK)
 }
