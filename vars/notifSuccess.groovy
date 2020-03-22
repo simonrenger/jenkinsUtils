4a5313ed4f65f63a6897a106fbd974950be387e1
Simon Renger <simon.renger@gmail.com>
2020-03-22 16:30:51 +0100

    added missing whitespace

--- vars/notifSuccess.groovy
+++ vars/notifSuccess.groovy
@@ -5,6 +5,6 @@ WEBHOOK
     discord.send(
         discord.createMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Last Perforce Revision: ${env.P4_CHANGELIST} Passed Building","ok",[
                     [name:"Last Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true]
-                ],"${CHANGE_URL}${env.P4_CHANGELIST}","Build URL: ${env.BUILD_URL} Change Url: ${CHANGE_URL}${env.P4_CHANGELIST}")
+                ],"${CHANGE_URL}${env.P4_CHANGELIST}","Build URL: ${env.BUILD_URL}\nChange Url: ${CHANGE_URL}${env.P4_CHANGELIST}")
                 ,WEBHOOK)
 }
