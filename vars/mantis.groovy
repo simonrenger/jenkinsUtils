0df936c509cbb2b9a7fefc58e2cf1ac070d0d67b
Simon Renger <simon.renger@gmail.com>
2020-03-23 19:11:24 +0100

    added mantis api calls

---
+++ vars/mantis.groovy
@@ -0,0 +1,37 @@
+import groovy.json.JsonOutput
+import hudson.model.*
+
+def token_ = ""
+def url_ = ""
+
+def setUrl(url){
+    url_ = url
+}
+def setToken(token){
+    token_ = token
+}
+
+def update(id,status,resolution,handler){
+
+def body = [
+    id: id,
+    status: [
+        name: status
+    ],
+    resolution: [
+        name: resolution
+    ]
+]
+
+    if(status == "assigned"){
+        body.handler = [
+            name: handler
+        ]
+    }
+
+    def bodyJson = JsonOutput.toJson(body).replace('"','""')
+    bat label: '', script: "curl --location --request PATCH \'${url_}/api/rest/issues/${id}\' ^
+    --header \'Authorization: ${token_}\' ^
+    --header \'Content-Type: application/json\' ^
+    --data-raw \'${bodyJson}\'"
+}
