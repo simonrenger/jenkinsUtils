805e2fff148973a80fe8d23519da20e0eff494aa
Simon Renger <simon.renger@gmail.com>
2020-03-24 17:36:32 +0100

    corrected repalcement func

--- vars/steam.groovy
+++ vars/steam.groovy
@@ -1,24 +1,24 @@
 def depotManifest(depotNumber,contentRoot,localPath="*",depotPath=".",recursive="1",exclude="*.pdb"){
     def depot_manifest = libraryResource 'de/simonrenger/depot_build.vdf.tpl'
-    depot_manifest = depot_manifest.replaceAll('[STEAM_DEPOT_ID]',depotNumber)
-    depot_manifest = depot_manifest.replaceAll('[CONTENT_ROOT]',contentRoot)
-    depot_manifest = depot_manifest.replaceAll('[STEAM_LOCAL_PATH]',localPath)
-    depot_manifest = depot_manifest.replaceAll('[STEAM_DEPOT_PATH]',depotPath)
-    depot_manifest = depot_manifest.replaceAll('[STEAM_DEPOT_RECURSIVE]',recursive)
-    depot_manifest = depot_manifest.replaceAll('[STEAM_DEPOT_EXCLUDE]',exclude)
+    depot_manifest = depot_manifest.replace('[STEAM_DEPOT_ID]',depotNumber)
+    depot_manifest = depot_manifest.replace('[CONTENT_ROOT]',contentRoot)
+    depot_manifest = depot_manifest.replace('[STEAM_LOCAL_PATH]',localPath)
+    depot_manifest = depot_manifest.replace('[STEAM_DEPOT_PATH]',depotPath)
+    depot_manifest = depot_manifest.replace('[STEAM_DEPOT_RECURSIVE]',recursive)
+    depot_manifest = depot_manifest.replace('[STEAM_DEPOT_EXCLUDE]',exclude)
     writeFile file: "depot_build_${depotNumber}.vdf", text: depot_manifest
 }
 
 def appManifest(appId,depotNumber,outputdir,steamBranch,isPreview="0",contentroot=""){
     def app_manifest = libraryResource 'de/simonrenger/app_build.vdf.tpl'
-    app_manifest = app_manifest.replaceAll('[STEAM_APP_ID]',appId)
-    app_manifest = app_manifest.replaceAll('[JOB_BASE_NAME]',JOB_NAME)
-    app_manifest = app_manifest.replaceAll('[BUILD_NUMBER]',BUILD_NUMBER)
-    app_manifest = app_manifest.replaceAll('[STEAM_IS_PREVIEW_BUILD]',isPreview)
-    app_manifest = app_manifest.replaceAll('[STEAM_BRANCH]',steamBranch)
-    app_manifest = app_manifest.replaceAll('[OUTPUT_DIR]',outputdir)
-    app_manifest = app_manifest.replaceAll('[CONTENT_ROOT]',contentroot)
-    app_manifest = app_manifest.replaceAll('[STEAM_DEPO_NUMBER]',depotNumber)
+    app_manifest = app_manifest.replace('[STEAM_APP_ID]',appId)
+    app_manifest = app_manifest.replace('[JOB_BASE_NAME]',JOB_NAME)
+    app_manifest = app_manifest.replace('[BUILD_NUMBER]',BUILD_NUMBER)
+    app_manifest = app_manifest.replace('[STEAM_IS_PREVIEW_BUILD]',isPreview)
+    app_manifest = app_manifest.replace('[STEAM_BRANCH]',steamBranch)
+    app_manifest = app_manifest.replace('[OUTPUT_DIR]',outputdir)
+    app_manifest = app_manifest.replace('[CONTENT_ROOT]',contentroot)
+    app_manifest = app_manifest.replace('[STEAM_DEPO_NUMBER]',depotNumber)
     writeFile file: "app_build_${appId}.vdf", text: app_manifest
 }
 
