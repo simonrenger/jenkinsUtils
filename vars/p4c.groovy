0cd4e0d0b8ef48298514733f92ce3483df284532
Simon Renger <simon.renger@gmail.com>
2020-03-27 15:05:11 +0100

    fixed writeable bug

--- vars/p4c.groovy
+++ vars/p4c.groovy
@@ -1,7 +1,7 @@
 import groovy.json.JsonSlurper
 
 def getChangelistDescription(id,credential,client,view_mapping){
-    def p4s = p4(credential: credential, workspace: manualSpec(charset: 'none', cleanup: false, name: client, pinHost: false, spec: clientSpec(allwrite: false, backup: true, changeView: '', clobber: false, compress: false, line: 'LOCAL', locked: false, modtime: false, rmdir: false, serverID: '', streamName: '', type: 'WRITABLE', view: view_mapping)))
+    def p4s = p4(credential: credential, workspace: manualSpec(charset: 'none', cleanup: false, name: client, pinHost: false, spec: clientSpec(allwrite: true, backup: true, changeView: '', clobber: false, compress: false, line: 'LOCAL', locked: false, modtime: false, rmdir: false, serverID: '', streamName: '', type: 'WRITABLE', view: view_mapping)))
     def list = p4s.run('describe', '-s', '-S', "${id}")
     def desc = ""
     for(def item : list) {
