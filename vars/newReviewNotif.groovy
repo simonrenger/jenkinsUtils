9768d5279e5206b6e91dc34fc28a31c6941acd49
Simon Renger <simon.renger@gmail.com>
2020-03-27 18:08:30 +0100

    added reviewID parameter

--- vars/newReviewNotif.groovy
+++ vars/newReviewNotif.groovy
@@ -1,12 +1,13 @@
 def call(
+review,
 REVIEW_URL,
 WEBHOOK,
 description = ""
 ){
     discord.send(
-             discord.createMessage("New/Updated Review: ${env.P4_REVIEW}","new",[
-                [name:"Perforce review",value:"${env.P4_REVIEW}",inline:true],
+             discord.createMessage("New/Updated Review: ${review}","new",[
+                [name:"Perforce review",value:"${review}",inline:true],
                 [name:"Jenkins Build",value:"${env.BUILD_NUMBER}",inline:true]
-            ],"${REVIEW_URL}${env.P4_REVIEW}","**New Review or Update**\nBuild URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${env.P4_REVIEW}\nReview Description:\n${description}")
+            ],"${REVIEW_URL}${review}","**New Review or Update\nBuild URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${review}\nReview Description:\n${description}")
             ,WEBHOOK)
 }
