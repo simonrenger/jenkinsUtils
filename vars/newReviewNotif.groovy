5959d3a1e6a1b5d1e6995f6ea5b8f10736494ded
Simon Renger <simon.renger@gmail.com>
2020-03-27 17:37:04 +0100

    changed new review message content

--- vars/newReviewNotif.groovy
+++ vars/newReviewNotif.groovy
@@ -4,9 +4,9 @@ WEBHOOK,
 description = ""
 ){
     discord.send(
-             discord.createMessage("New Review: ${env.P4_REVIEW}","new",[
+             discord.createMessage("New/Updated Review: ${env.P4_REVIEW}","new",[
                 [name:"Perforce review",value:"${env.P4_REVIEW}",inline:true],
                 [name:"Jenkins Build",value:"${env.BUILD_NUMBER}",inline:true]
-            ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${env.P4_REVIEW}\nReview Description:\n${description}")
+            ],"${REVIEW_URL}${env.P4_REVIEW}","**New Review or Update**\nBuild URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${env.P4_REVIEW}\nReview Description:\n${description}")
             ,WEBHOOK)
 }
