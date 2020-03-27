b2a443a05abf297a3bc15929fb4cc5066bdc921b
Simon Renger <simon.renger@gmail.com>
2020-03-27 18:11:58 +0100

    fix little display bug

--- vars/newReviewNotif.groovy
+++ vars/newReviewNotif.groovy
@@ -8,6 +8,6 @@ description = ""
              discord.createMessage("New/Updated Review: ${review}","new",[
                 [name:"Perforce review",value:"${review}",inline:true],
                 [name:"Jenkins Build",value:"${env.BUILD_NUMBER}",inline:true]
-            ],"${REVIEW_URL}${review}","**New Review or Update\nBuild URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${review}\nReview Description:\n${description}")
+            ],"${REVIEW_URL}${review}","**New Review or Update**\nBuild URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${review}\nReview Description:\n${description}")
             ,WEBHOOK)
 }
