b3ddb7176202d407ddde25016e5edfa81b8a3c52
Simon Renger <simon.renger@gmail.com>
2020-03-27 18:13:43 +0100

    changed var name

--- vars/newReviewNotif.groovy
+++ vars/newReviewNotif.groovy
@@ -1,13 +1,13 @@
 def call(
-review,
+reviewId,
 REVIEW_URL,
 WEBHOOK,
 description = ""
 ){
     discord.send(
-             discord.createMessage("New/Updated Review: ${review}","new",[
-                [name:"Perforce review",value:"${review}",inline:true],
+             discord.createMessage("New/Updated Review: ${reviewId}","new",[
+                [name:"Perforce review",value:"${reviewId}",inline:true],
                 [name:"Jenkins Build",value:"${env.BUILD_NUMBER}",inline:true]
-            ],"${REVIEW_URL}${review}","**New Review or Update**\nBuild URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${review}\nReview Description:\n${description}")
+            ],"${REVIEW_URL}${reviewId}","**New Review or Update**\nBuild URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${reviewId}\nReview Description:\n${description}")
             ,WEBHOOK)
 }
