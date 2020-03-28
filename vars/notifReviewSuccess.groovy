18f1c2cbb36598bff2ba939f25f3722448c99087
Simon Renger <simon.renger@gmail.com>
2020-03-28 18:16:24 +0100

    changed wording

--- vars/notifReviewSuccess.groovy
+++ vars/notifReviewSuccess.groovy
@@ -7,6 +7,6 @@ description = ""
         discord.createMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Review: ${env.P4_REVIEW} Passed Building","ok",[
                     [name:"Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true],
                     [name:"Perforce Review",value:"${env.P4_REVIEW}",inline:true]
-                ],"${REVIEW_URL}${env.P4_REVIEW}","Build URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${env.P4_REVIEW}\nReview Description:\n${description}")
+                ],"${REVIEW_URL}${env.P4_REVIEW}","**Status: Success**\nBuild URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${env.P4_REVIEW}\nReview Description:\n${description}")
                 ,WEBHOOK)
 }
