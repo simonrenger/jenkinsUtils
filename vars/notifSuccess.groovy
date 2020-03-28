a68e8e2e0b59f7ad5b27b4c1ca70099cf089c062
Simon Renger <simon.renger@gmail.com>
2020-03-28 19:34:43 +0100

    added extra option of desc to notif

--- vars/notifSuccess.groovy
+++ vars/notifSuccess.groovy
@@ -1,10 +1,11 @@
 def call(
 CHANGE_URL,
-WEBHOOK
+WEBHOOK,
+description = ""
 ){
     discord.send(
         discord.createMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Last Perforce Revision: ${env.P4_CHANGELIST} Passed Building","ok",[
                     [name:"Last Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true]
-                ],"${CHANGE_URL}${env.P4_CHANGELIST}","Build URL: ${env.BUILD_URL}\nChange Url: ${CHANGE_URL}${env.P4_CHANGELIST}")
+                ],"${CHANGE_URL}${env.P4_CHANGELIST}","Build URL: ${env.BUILD_URL}\nChange Url: ${CHANGE_URL}${env.P4_CHANGELIST}\n${description}")
                 ,WEBHOOK)
 }
