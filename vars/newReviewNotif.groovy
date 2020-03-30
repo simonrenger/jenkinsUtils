faa59de47fdec6329c5751bb522d0820d660708c
Simon Renger <simon.renger@gmail.com>
2020-03-30 21:52:16 +0200

    added extra param for review status update

--- vars/newReviewNotif.groovy
+++ vars/newReviewNotif.groovy
@@ -2,10 +2,11 @@ def call(
 reviewId,
 REVIEW_URL,
 WEBHOOK,
-description = ""
+description = "",
+reviewStatusParam = "New/Updated"
 ){
     discord.send(
-             discord.createMessage("New/Updated Review: ${reviewId}","new",[
+             discord.createMessage("${reviewStatusParam} Review: ${reviewId}","new",[
                 [name:"Perforce review",value:"${reviewId}",inline:true],
                 [name:"Jenkins Build",value:"${env.BUILD_NUMBER}",inline:true]
             ],"${REVIEW_URL}${reviewId}","**New Review or Update**\nBuild URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${reviewId}\nReview Description:\n${description}")
