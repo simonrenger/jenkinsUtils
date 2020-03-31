840802e4bfc32e36346ffd01348407b6dad4f397
Simon Renger <simon.renger@gmail.com>
2020-03-31 22:26:33 +0200

    added executeMethodParams

--- vars/unity.groovy
+++ vars/unity.groovy
@@ -4,7 +4,14 @@ def setRoot(root){
     unity_ = unity_
 }
 
-// Build a release version of the VRTower Unity project using the projects C# build script.
-def build(projectPath,executeMethod,level,output, logFile = "log{$env.JOB_BASE_NAME}${env.BUILD_NUMBER}.txt"){
-    bat label: 'build unity', script: "CALL \"${unity_}\"  -batchmode -quit -projectPath \"${projectPath}\" -logFile \"${logFile}\" -executeMethod ${executeMethod} \"${level}\" \"${output}\""
+// Build a release version of a Unity project using the projects C# build script.
+def build(projectPath,executeMethod,executeMethodParams = [], logFile = "log{$env.JOB_BASE_NAME}${env.BUILD_NUMBER}.txt"){
+    def params = ""
+    if(executeMethodParams.length > 0){
+        executeMethodParams.each{
+            -> value
+            params = "${params} \"${value}\""
+        }
+    }
+    bat label: 'build unity', script: "CALL \"${unity_}\"  -batchmode -quit -projectPath \"${projectPath}\" -logFile \"${logFile}\" -executeMethod ${executeMethod} ${params}"
 }
