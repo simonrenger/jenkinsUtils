b0cffabbc47b4d685aa3ceadd84853f96c019153
Simon Renger <simon.renger@gmail.com>
2020-04-01 19:40:12 +0200

    propery rename file utility to dirExists

--- vars/dirExists.groovy
+++ vars/dirExists.groovy
@@ -1,5 +1,5 @@
 // why? This function allowes for checking outside of the workspace while `fileExists` only within the boundaries of the workspace
-def dirExists(folder){
+def call(folder){
     def result = powershell(label: 'check if folder exsits', returnStdout: true, script: "Test-Path '${folder}'")
     if(result.replaceAll("\\s","").equals("True")){
         return true
