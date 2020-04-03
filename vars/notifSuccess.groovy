b6629d14abb06ba05230f33251d745573aa5af4c
Simon Renger <simon.renger@gmail.com>
2020-04-03 21:34:56 +0200

    build works notification

--- vars/notifSuccess.groovy
+++ vars/notifSuccess.groovy
@@ -6,6 +6,6 @@ description = ""
     discord.send(
         discord.createMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Last Perforce Revision: ${env.P4_CHANGELIST} Passed Building","ok",[
                     [name:"Last Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true]
-                ],"${CHANGE_URL}${env.P4_CHANGELIST}","Build URL: ${env.BUILD_URL}\nChange Url: ${CHANGE_URL}${env.P4_CHANGELIST}\n${description}")
+                ],"${CHANGE_URL}${env.P4_CHANGELIST}","**Status: Success**\nBuild URL: ${env.BUILD_URL}\nChange Url: ${CHANGE_URL}${env.P4_CHANGELIST}\n${description}")
                 ,WEBHOOK)
 }
