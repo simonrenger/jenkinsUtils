13043dba0acafb893c8198ca9ce0a99b256cf5b0
Simon Renger <simon.renger@gmail.com>
2020-04-06 16:34:20 +0200

    added single quoats to maybe fix space in paths

--- vars/mkdir.groovy
+++ vars/mkdir.groovy
@@ -1,6 +1,6 @@
 def call(dir,throwError = true){
     if(!dirExists(dir)){
-        powershell label: 'create dir', script: "mkdir ${dir}"
+        powershell label: 'create dir', script: "mkdir '${dir}'"
     }else{
         echo "Cannot create Dir. It already exists!"
         if(throwError){
