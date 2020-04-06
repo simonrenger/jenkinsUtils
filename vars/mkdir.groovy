70fced784d745e26e7cb916f2c24be3ab750031f
Simon Renger <simon.renger@gmail.com>
2020-04-06 17:15:13 +0200

    fixed bug varified and tested #build87

--- vars/mkdir.groovy
+++ vars/mkdir.groovy
@@ -1,6 +1,5 @@
 def call(dir,name,throwError = true){
     if(!dirExists(dir)){
-        echo 
         powershell label: 'create dir', script: "mkdir -Path '${dir}' -Name '${name}'"
     }else{
         echo "Cannot create Dir. It already exists!"
