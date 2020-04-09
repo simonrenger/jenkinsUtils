abe190501716f7ffce532498aaea4a340f44313e
Simon Renger <simon.renger@gmail.com>
2020-04-09 19:04:57 +0200

    changed review desc

--- vars/newReviewNotif.groovy
+++ vars/newReviewNotif.groovy
@@ -9,6 +9,6 @@ reviewStatusParam = "New/Updated"
              discord.createMessage("${reviewStatusParam} Review: ${reviewId}","new",[
                 [name:"Perforce review",value:"${reviewId}",inline:true],
                 [name:"Jenkins Build",value:"${env.BUILD_NUMBER}",inline:true]
-            ],"${REVIEW_URL}${reviewId}","**${reviewStatusParam} Review**\nBuild URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${reviewId}\nReview Description:\n${description}")
+            ],"${REVIEW_URL}${reviewId}","**${reviewStatusParam} Review**\nBuild URL: ${env.BUILD_URL}\nReview Url: ${REVIEW_URL}${reviewId}\n\n${description}")
             ,WEBHOOK)
 }
