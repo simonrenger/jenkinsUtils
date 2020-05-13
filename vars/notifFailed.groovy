60da6f8f34fb2db020e0009ba2ee3bbb074cf9f0
Simon Renger <simon.renger@gmail.com>
2020-05-13 21:52:20 +0200

    [*] added build stage to notification

--- vars/notifFailed.groovy
+++ vars/notifFailed.groovy
@@ -3,9 +3,13 @@ CHANGE_URL,
 WEBHOOK,
 description = ""
 ){
+    def build_stage = ""
+    if(log.lastStage() != null){
+        build_stage = "Build Stage: ${log.lastStage()}\n"
+    }
     discord.send(
              discord.createMessage(":x: Build #${env.BUILD_NUMBER} - Last Perforce Revision: ${env.P4_CHANGELIST} Failed Building","failed",[
                 [name:"Last Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true]
-            ],"${CHANGE_URL}${env.P4_CHANGELIST}","**Status: Failed**\nBuild URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nChange Url: ${CHANGE_URL}${env.P4_CHANGELIST}\n${description}")
+            ],"${CHANGE_URL}${env.P4_CHANGELIST}","**Status: Failed**\n${build_stage}Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nChange Url: ${CHANGE_URL}${env.P4_CHANGELIST}\n${description}")
             ,WEBHOOK)
 }
--- vars/notifReviewFailed.groovy
+++ vars/notifReviewFailed.groovy
@@ -3,10 +3,14 @@ REVIEW_URL,
 WEBHOOK,
 description = ""
 ){
+    def build_stage = ""
+    if(log.lastStage() != null){
+        build_stage = "Build Stage: ${log.lastStage()}\n";
+    }
     discord.send(
              discord.createMessage(":x: Build #${env.BUILD_NUMBER} - Review: ${env.P4_REVIEW} Failed Building","failed",[
                 [name:"Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true],
                 [name:"Perforce Review",value:"${env.P4_REVIEW}",inline:true]
-            ],"${REVIEW_URL}${env.P4_REVIEW}","**Status: Failed**\nBuild URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${REVIEW_URL}${env.P4_REVIEW}\nReview Description:\n${description}")
+            ],"${REVIEW_URL}${env.P4_REVIEW}","**Status: Failed**\n${build_stage}Build URL: ${env.BUILD_URL}\nBuild Log URL: ${env.BUILD_URL}console/ \nBuild Parsed Log URL: ${env.BUILD_URL}parsed_console/ \nReview Url: ${REVIEW_URL}${env.P4_REVIEW}\nReview Description:\n${description}")
             ,WEBHOOK)
 }
